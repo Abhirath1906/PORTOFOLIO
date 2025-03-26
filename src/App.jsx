69fb794643659e974155217ba8490c06417985da
Abhirath1906 <abhirath1906@gmail.com>
2025-03-26 03:11:34 -0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Portofolioo from './pages/Portofolioo'
-import Home from './pages/Home'
+// import Portofolioo from './pages/Portofolioo'
+// import Home from './pages/Home'
 import About from './pages/About'
 import Contact from './pages/Contact'
 import Skills from './pages/Skills'
