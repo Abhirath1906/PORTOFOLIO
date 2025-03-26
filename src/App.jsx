e747e073f595d899a16ce20b5d31a85af11b3b8c
Abhirath1906 <abhirath1906@gmail.com>
2025-03-26 06:01:51 -0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
 
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-// import Portofolioo from './pages/Portofolioo'
-// import Home from './pages/Home'
-// import About from './pages/About'
-// import Contact from './pages/Contact'
-// import Skills from './pages/Skills'
-// import Portocv from './pages/Portocv'
+import Portofolioo from './pages/Portofolioo'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Skills from './pages/Skills'
+import Portocv from './pages/Portocv'
 function App(){
 
 return(
     <>
 <Router>
     <Routes>
-        {/* <Route path='/' element={<Portofolioo/>}/>
+        <Route path='/' element={<Portofolioo/>}/>
         <Route path='/home' element={<Home/>}/>
         <Route path='/about' element={<About/>}/>
         <Route path='/contact' element={<Contact/>}/>
         <Route path='/skills' element={<Skills/>}/>
-        <Route path='/portocv' element={<Portocv/>}/> */}
+        <Route path='/portocv' element={<Portocv/>}/>
     </Routes>
 </Router>
     </>
